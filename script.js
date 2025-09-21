a5b77c759326f011aa8acf447f46cf7d59e7b6a6
Samriddhi2913 <samriddhisingh3004@gmail.com>
2025-09-21 12:00:12 +0530

    Update JS File

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ const choices = document.querySelectorAll(".choice");
 const genCompChoice = ()=>{
     //rock, paper, scissors
     const options = ["rock", "paper", "scissors"]
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
 }
 
-const playGame=()=>{
+const playGame=(userChoice)=>{
     console.log("user choice is: ", userChoice);
     // Generate Computer Choice
+    const compChoice = genCompChoice();
+    console.log("Computer choice is: ",compChoice);
 }
 
 choices.forEach((choice)=>{
     // console.log(choice);
     choice.addEventListener("click", ()=>{
         const userChoice = choice.getAttribute("id");
-        console.log("Choice was clicked", userChoice);
+        // console.log("Choice was clicked", userChoice);
         playGame(userChoice);
     });
 });
